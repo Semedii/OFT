832374bab63e563fb523c18a841df4b130c91e41
abdulraheemf <57996164+abdulraheemf@users.noreply.github.com>
2021-11-18 14:18:42 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,196 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'homeScreens/Widgets.dart';
+import 'homeScreens/drawer.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'OTF Score',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(), //MyHomePage(title: 'OTF Score'),
     );
   }
 }
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
 
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+//This splash Screen is the welcome screen. It will only last 2 seconds
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  //this function is gonna count the time. the 2seconds
+  void initState() {
+    super.initState();
+    new Timer(const Duration(seconds: 2), onclose);
+    //Timer(Duration(seconds: 3), onclose());
+  }
+  //this function will destroy the splash screen when the time is up. and it will direct you to the homepage
+  void onclose(){
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => MyHomePage(title: 'OTF Score')));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+
+              child: Image.asset("images/wal.jpg", fit: BoxFit.cover,),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircularProgressIndicator(
+                          backgroundColor: Colors.white,
+                        ),
+                        Text("Connecting to OFTScore", style: TextStyle(color: Colors.white, fontSize: 20),)
+                      ],
+                    ),
+                  ),
+                )
+              ],)
+          ],)
+    );
+  }
+}
+//this homepage screen has the following
+// A stack. and inside the stack are a listview where you can add the new matches
+// the matches is a class with two parameters (the two teams)
+// and also there Row with three different buttons in the stack.
+//those three different buttons will navigate to three different screens
+//inside the appbar there is settings which will navigate to setting screen and...
+//..main menu where you can login or sign up.
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        drawer: DrawerScreen(),
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: (){
+
+            }, icon: Icon(Icons.settings))
           ],
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body:  Stack(
+          children: [
+            ListView(
+
+              physics: BouncingScrollPhysics(), // this just for bouncing when scrolled
+              children: [
+                matches("Man United","Liverpool"),
+                matches("Man United","Liverpool"),
+                matches("Man United","Liverpool"),
+                matches("Man United","Liverpool"),
+
+              ],),
+            Positioned.fill(
+              // bottom: 20,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+
+                  child:  Row(
+
+                    children: [
+                      Expanded(
+                        child: Container(
+                            height: 60,
+
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.article),
+                                Text("News", style: TextStyle( color: Colors.white,
+                                  fontSize: 16,),)
+                              ],
+                            )
+
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                            height: 60,
+                            width: 0.4*390,
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.article),
+                                Text("Discussion", style: TextStyle( color: Colors.white,
+                                  fontSize: 16,),)
+                              ],
+                            )
+
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                            height: 60,
+                            width: 0.4*390,
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.favorite),
+                                Text("Favourites", style: TextStyle( color: Colors.white,
+                                  fontSize: 16,),)
+                              ],
+                            )
+
+                        ),
+                      )
+                    ],),
+                ))
+          ],
+        )
+
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
