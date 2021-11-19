52300991debd47e00cc909cb13cc73ba14cd5725
adamce7 <94012490+adamce7@users.noreply.github.com>
2021-11-20 00:21:32 +0300

    Add files via upload

--- lib/Favourite Screen/fav.dart
+++ lib/Favourite Screen/fav.dart
@@ -1,51 +1,178 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class DrawerScreen extends StatelessWidget {
-  const DrawerScreen({ Key? key }) : super(key: key);
+import 'package:flutter/painting.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class fav extends StatelessWidget {
+  const fav({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: SafeArea(
-        child: Column(children: [
-
-          Row(
-            children: [
-
-              Expanded(
-                child: Container(
-                    height: 200,
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF12c387),
+        title: const Text(
+          'Favourites',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 25.0,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                'Edit',
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.add_circle_outline_rounded),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    height: double.infinity,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(10.0),
                     decoration: BoxDecoration(
-                      //this color is gradient just for decoration
-                      gradient: LinearGradient(
-
-                          colors: [Color(0xFF000000), Color(0xFF161315), Color(0xFF241f23),
-                            Color(0xFF302c33), Color(0xFF3b3a45), Color(0xFF464955),Color(0xFF515965),
-                            Color(0xFF5d6974), Color(0xFF717d84),Color(0xFF889295),
-                          ]),
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(50, 70, 50, 50),
-                      child: Text("Login Page", style: TextStyle(fontSize: 40, color: Colors.white)),
-                    )),
-              )
-            ],
-          ),
-          Card(
-            child: ListTile(
-              leading:Icon(Icons.login),
-              title: Text("Login"),
-            ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(10.0),
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: <Widget>[
+                         Row(
+                           children: [
+                             Image.asset(
+                               'images/city.png',
+                               height: 50.0,
+                               width: 50.0,
+                               fit: BoxFit.contain,
+                              alignment: Alignment.topLeft,
+                             ),
+                           ],
+                         ),
+                         SizedBox(
+                           height: 12.0,
+                         ),
+                         Row(
+                           children: [
+                             Text('Man City', style: TextStyle(fontSize: 20.0, color: Colors.white),),
+                           ],
+                         ),
+                         SizedBox(
+                           height: 15.0,
+                         ),
+                         Row(
+                           children: [
+                             Icon(FontAwesomeIcons.planeDeparture),
+                             SizedBox(
+                               width: 10.0,
+                             ),
+                             Text('Everton', style: TextStyle(fontSize: 15.0),),
+                           ],
+                         ),
+                         Row(
+                           children: [
+                             Text('Sun,21 Nov, 5:00pm'),
+                           ],
+                         ),
+                       ],
+                     ),
+                   ),
 
-          ),
-          Card(
-            child: ListTile(
-              leading:Icon(Icons.app_registration_outlined),
-              title: Text("Signup"),
+                  ),
+                ),
+                Expanded(
+                  child: Reusable(),
+                ),
+              ],
             ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Reusable(),
+                ),
+                Expanded(
+                  child: Reusable(),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Reusable(),
+                ),
+                Expanded(
+                  child: Reusable(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.futbol),
+            label: 'Matches',
+            backgroundColor: Color(0xff12c387),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.newspaper),
+            label: 'News',
+            backgroundColor: Color(0xFFf0725c),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.trophy),
+            label: 'Leagues',
+            backgroundColor: Color(0xFFf0725c),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.star),
+            label: 'Favorites',
+            backgroundColor: Color(0xfff0725c),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'More',
+            backgroundColor: Color(0xffcced00),
+          ),
+        ],
+        selectedItemColor: Colors.yellowAccent,
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
 
-          )
-        ]),
+class Reusable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.cyan,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
